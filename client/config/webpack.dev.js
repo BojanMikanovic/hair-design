7f3a76cf5cbe078210254a4d469f6b8b91d3062d
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2026-04-23 22:35:03 +0200

    Add CORS headers to webpack development server configuration

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -25,7 +25,12 @@ module.exports = async () => {
          server:{
             type: 'https',
             options: ssl
-         }
+         },
+         headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            }
       },
    });
 };
